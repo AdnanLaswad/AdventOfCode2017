cacc65f466131dddfb70bb2b6cccd5f585adbfa1
Carsten Koenig <Carsten@gettingsharper.de>
2017-12-01 06:52:21 +0100

    day 1: star 2

--- Day1/Day1.hs
+++ Day1/Day1.hs
@@ -8,6 +8,8 @@ main = do
   inp <- readInput
   let sol = solution inp
   print sol
+  let sol2 = solution2 inp
+  print sol2
 
 
 solution :: [Integer] -> Integer
@@ -16,6 +18,13 @@ solution xs = sum . catMaybes . map goodPairs $ pairs (xs ++ [head xs])
           | a == b    = Just a
           | otherwise = Nothing
 
+
+solution2 :: [Integer] -> Integer
+solution2 xs = sum . catMaybes . map goodPairs $ zipWith (,) xs (drop mid xs ++ take mid xs)
+  where goodPairs (a,b)
+          | a == b    = Just a
+          | otherwise = Nothing
+        mid = length xs `div` 2
 
 pairs :: [a] -> [(a,a)]
 pairs [] = []
