4c664c596b505de66e0c1a13ad39d80d95be64a9
Carsten Koenig <Carsten@gettingsharper.de>
2017-12-04 06:26:05 +0100

    day 4 star 2

--- Day4/Day4.hs
+++ Day4/Day4.hs
@@ -2,21 +2,33 @@
 
 module Main where
 
-import Data.List (sort, group)
+import Data.List (sort, group, nub)
 
 main :: IO ()
 main = do
   inp <- readInput
-  
-  let part1 = length . filter isValidPassphrase $ inp
+
+  let part1 = length . filter noDuplicateWords $ inp
   putStrLn $ "part 1: " ++ show part1
+
+  let part2 = length . filter isValidPassphrase $ inp
+  putStrLn $ "part 2: " ++ show part2
+
 
 
 type Input = [String] 
 
 
 isValidPassphrase :: [String] -> Bool
-isValidPassphrase = all ((== 1) . length) . group . sort
+isValidPassphrase xs = noDuplicateWords xs && noAnagrams xs
+
+
+noAnagrams :: [String] -> Bool
+noAnagrams xs = length xs == (length $ nub $ map sort xs)
+
+
+noDuplicateWords :: [String] -> Bool
+noDuplicateWords = all ((== 1) . length) . group . sort
 
 
 readInput :: IO [Input]
