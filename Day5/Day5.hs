f20ba0d9f2db7ba5454d5ad61fb4003ace907f03
Carsten Koenig <Carsten@gettingsharper.de>
2017-12-05 06:33:15 +0100

    day 5 star 2 and refactoring

--- Day5/Day5.hs
+++ Day5/Day5.hs
@@ -11,16 +11,72 @@ main :: IO ()
 main = do
   inp <- readInput
   maschine <- init <$> readInput
-  let steps = process maschine
-  putStrLn $ "part 1: machine took " ++ show steps ++ " steps"
 
+  let steps1 = process incr maschine
+  putStrLn $ "part 1: machine took " ++ show steps1 ++ " steps"
+
+  let steps2 = process strange maschine
+  putStrLn $ "part 2: machine took " ++ show steps2 ++ " steps"
+
+
+example :: Zipper Jump
+example = init $ map Jump [0,3,0,1,(-3)]
+
+
+----------------------------------------------------------------------
+-- Input
 
 type Input = [Jump]
 
+readInput :: IO Input
+readInput = map (Jump . read). lines <$> readFile "input.txt"
+
+
+----------------------------------------------------------------------
+-- algorithm
 
 data Jump = Jump Int
   deriving Show
 
+
+process :: OffsetJump -> Zipper Jump -> Int
+process upd z =
+  if isStopped z then 0 else 1 + process upd (step upd z)
+
+
+step :: OffsetJump -> Zipper Jump -> Zipper Jump
+step upd z =
+  case current z of
+    Nothing       -> z
+    Just (Jump n) ->
+      let z' = update (upd n) z
+          f  = if n >= 0 then moveRight else moveLeft
+      in ntimes f (abs n) z'
+
+
+isStopped :: Zipper Jump -> Bool
+isStopped = isNothing . current
+
+
+init :: Input -> Zipper Jump
+init jmps = Zipper [] jmps
+
+
+type OffsetJump = Int -> Jump -> Jump
+
+
+incr :: OffsetJump
+incr _ (Jump n) = Jump (n+1)
+
+
+strange :: OffsetJump
+strange n (Jump j)
+  | n >= 3    = Jump (j-1)
+  | otherwise = Jump (j+1)
+
+
+----------------------------------------------------------------------
+-- Zipper
 
 data Zipper a =
   Zipper { left :: [a]
@@ -29,41 +85,9 @@ data Zipper a =
   deriving Show
 
 
-init :: Input -> Zipper Jump
-init jmps = Zipper [] jmps
-
-
-process :: Zipper Jump -> Int
-process z =
-  if isStopped z then 0 else 1 + process (step z)
-
-
-step :: Zipper Jump -> Zipper Jump
-step z =
-  case current z of
-    Nothing       -> z
-    Just (Jump n) ->
-      let z' = update incr z
-          f  = if n >= 0 then moveRight else moveLeft
-      in ntimes f (abs n) z'
-
-
-ntimes :: (a -> a) -> Int -> a -> a
-ntimes _ 0 a = a
-ntimes f n a = ntimes f (n-1) (f a)
-
-
-incr :: Jump -> Jump
-incr (Jump n) = Jump (n+1)
-
-
 update :: (a -> a) -> Zipper a -> Zipper a
 update _ z@(Zipper _ []) = z
 update f (Zipper ls (c:rs)) = Zipper ls (f c : rs)
-
-
-isStopped :: Zipper a -> Bool
-isStopped = isNothing . current
 
 
 current :: Zipper a -> Maybe a
@@ -81,9 +105,11 @@ moveLeft z@(Zipper [] _) = z
 moveLeft (Zipper (c:ls) rs) = Zipper ls (c:rs)
 
 
-readInput :: IO Input
-readInput = map (Jump . read). lines <$> readFile "input.txt"
+----------------------------------------------------------------------
+-- helper
+
+ntimes :: (a -> a) -> Int -> a -> a
+ntimes _ 0 a = a
+ntimes f n a = ntimes f (n-1) (f a)
 
 
-example :: Zipper Jump
-example = init $ map Jump [0,3,0,1,(-3)]
