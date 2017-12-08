324bb30b4b9a188d12c03c5a2de6eb8483c77a22
Carsten König <carsten@gettingsharper.de>
2017-12-08 19:16:09 +0100

    day 7: fixed missing caes

--- Day7/Day7.hs
+++ Day7/Day7.hs
@@ -4,7 +4,7 @@ module Main where
 
 import Data.Function (on)
 import Data.List (foldl', groupBy, sortBy)
-import Data.Maybe (mapMaybe)
+import Data.Maybe (mapMaybe, fromJust)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 
@@ -27,7 +27,7 @@ main = do
   putStrLn $ "part1: " ++ base
 
   let m = buildMap inp
-      unb = findUnbalance m base 0
+      unb = findUnbalance m base Nothing
 
   putStrLn $ "part2: " ++ show unb
 
@@ -44,7 +44,7 @@ findBase inp =
 ----------------------------------------------------------------------
 -- part 2
 
-findUnbalance :: Map Name Program -> Name -> Int -> Int
+findUnbalance :: Map Name Program -> Name -> Maybe Int -> Int
 findUnbalance m b targetWeight =
   case Map.lookup b m of
     Nothing -> error "base not found"
@@ -53,14 +53,24 @@ findUnbalance m b targetWeight =
           grps = sortBy (compare `on` length) . groupBy ((==) `on` snd) $ sortBy (compare `on` snd) ws
       in case grps of
            -- if there are no children then the node itself is the wrong weight
-           []                    -> targetWeight
+           -- there should better be a targetWeight!
+           []                    -> fromJust targetWeight
            -- if all children have the same weight then we have to adapt the node weight
-           [xs]                  -> let supW = sum (snd <$> xs) in targetWeight - supW
+           -- again targetWeigth is neccessary
+           [xs]                  -> let supW = sum (snd <$> xs) in fromJust targetWeight - supW
+           -- if there are only two children then the good one must be half the remaining targetWeight
+           -- if there is no targetValue yet we are out of luck
+           [[(ca,wa)],[(cb,wb)]]
+             | Just wa == targetChildWeight p -> findUnbalance m cb (Just wa)
+             | Just wb == targetChildWeight p -> findUnbalance m ca (Just wb)
+             | otherwise                      -> error "don't know which way to go - shitty config man"
            -- if there is one child that has a different weight to all the others we
            -- have to adapt it's weight to be the same as the other childrens weight
-           [[(cont,_)], (_,w):_] -> findUnbalance m cont w
+           [[(cont,_)], (_,w):_] -> findUnbalance m cont (Just w)
            -- in all other cases the input have to be malformed
            _ -> error ("shitty config at " ++ b)
+  where
+    targetChildWeight p = fmap (\tw -> (tw - weight p)`div` 2) targetWeight
 
 
 ----------------------------------------------------------------------
