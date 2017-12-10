30935c1e6302750b57a18322042a54937c10d52f
Carsten König <carsten@gettingsharper.de>
2017-12-10 17:26:44 +0100

    prepared day 12

---
+++ Day12/Day12.hs
@@ -0,0 +1,16 @@
+-- Solution to Day 12 of the Advent Of Code 2017
+
+module Main where
+
+
+main :: IO ()
+main = do
+  inp <- readInput
+  putStrLn $ "solve me"
+
+
+type Input = ()
+
+
+readInput :: IO Input
+readInput = const () <$> readFile "input.txt"
