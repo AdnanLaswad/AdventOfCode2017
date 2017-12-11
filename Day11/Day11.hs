ff36d534858f2b5471d063ee401593d94c195185
Carsten Koenig <Carsten@gettingsharper.de>
2017-12-11 06:40:54 +0100

    day 11 both stars

--- Day11/Day11.hs
+++ Day11/Day11.hs
@@ -2,15 +2,80 @@
 
 module Main where
 
+import Data.List (foldl', scanl')
 
 main :: IO ()
 main = do
   inp <- readInput
-  putStrLn $ "solve me"
+  putStrLn $ "part 1: " ++ show (part1 inp)
+  putStrLn $ "part 2: " ++ show (part2 inp)
 
 
-type Input = ()
+type Input = [Move]
+
+
+data Move
+  = North
+  | NorthEast
+  | SouthEast
+  | South
+  | SouthWest
+  | NorthWest
+  deriving Show
+
+
+type Coord = (Int,Int)
+
+
+part1 :: [Move] -> Int
+part1 = hexDist start . moves start
+
+
+part2 :: [Move] -> Int
+part2 = maximum . map (hexDist start) . coords start
+
+
+start :: Coord
+start = (0,0)
+
+move :: Move -> Coord -> Coord
+move North (x,y)     = (x,y+1)
+move NorthEast (x,y) = (x+1,y+1)
+move SouthEast (x,y) = (x+1,y)
+move South (x,y)     = (x,y-1)
+move SouthWest (x,y) = (x-1,y-1)
+move NorthWest (x,y) = (x-1,y)
+
+
+moves :: Coord -> [Move] -> Coord
+moves start = foldl' (flip move) start
+
+
+coords :: Coord -> [Move] -> [Coord]
+coords start = scanl' (flip move) start
+
+
+hexDist :: Coord -> Coord -> Int
+hexDist (x,y) (x',y') =
+  let dX = x - x'
+      dY = y -y'
+      dD = dY - dX
+  in maximum $ map abs [dX,dY,dD]
+
+
+instance Read Move where
+  readsPrec _ ('n':'e':rest) = [(NorthEast, rest)]
+  readsPrec _ ('n':'w':rest) = [(NorthWest, rest)]
+  readsPrec _ ('n':rest)     = [(North, rest)]
+  readsPrec _ ('s':'e':rest) = [(SouthEast, rest)]
+  readsPrec _ ('s':'w':rest) = [(SouthWest, rest)]
+  readsPrec _ ('s':rest)     = [(South, rest)]
+  readsPrec _ _              = []
 
 
 readInput :: IO Input
-readInput = const () <$> readFile "input.txt"
+readInput = readMoves <$> readFile "input.txt"
+
+
+readMoves :: String -> [Move]
+readMoves inp = read ('[' : inp ++ "]")
