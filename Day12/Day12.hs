5f9e1b3023de154d77e5661b8395cbfc338e2ef5
Carsten Koenig <Carsten@gettingsharper.de>
2017-12-12 06:20:04 +0100

    day12 star 1

--- Day12/Day12.hs
+++ Day12/Day12.hs
@@ -2,15 +2,75 @@
 
 module Main where
 
+import Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as Map
+
+import Data.Maybe (mapMaybe, fromMaybe)
+
+import Parser
+
 
 main :: IO ()
 main = do
   inp <- readInput
-  putStrLn $ "solve me"
+  let g = buildGraph inp
+  
+  putStrLn $ "part 1: " ++ show (part1 g)
 
 
-type Input = ()
+type Input = [(Id, [Id])]
+
+
+type Connection = (Id, Id)
+
+type Id = Int
+
+type Graph = IntMap [Id]
+
+
+part1 :: Graph -> Int
+part1 g = length $ epsClosure g 0
+
+
+epsClosure :: Graph -> Id -> [Id]
+epsClosure graph i = go [] [i]
+  where
+    go visited [] = visited
+    go visited (i:is) =
+      if i `elem` visited
+      then go visited is
+      else go (i:visited) (is ++ connections graph i)
+
+
+connections :: Graph -> Id -> [Id]
+connections g i = fromMaybe [] $ Map.lookup i g
+
+buildGraph :: Input -> Graph
+buildGraph inp = foldr insertCon emptyGraph [ (f,t) | (f,ts) <- inp, t <- ts ]
+
+
+emptyGraph :: Graph
+emptyGraph = Map.empty
+
+
+insertCon :: Connection -> Graph -> Graph
+insertCon (from,to) =
+  Map.insertWith (++) from [to]
+  . Map.insertWith (++) to [from]
 
 
 readInput :: IO Input
-readInput = const () <$> readFile "input.txt"
+readInput = mapMaybe (eval lineP) . lines <$> readFile "input.txt"
+
+
+lineP :: Parser (Id, [Id])
+lineP = (,) <$> idP <*> listP
+
+
+idP :: Parser Id
+idP = parseInt <* ignoreWhiteSpace
+
+listP :: Parser [Id]
+listP = parseString "<->" *> ignoreWhiteSpace *>
+        (parseInt `parseSepBy` parseString ", ")
+
