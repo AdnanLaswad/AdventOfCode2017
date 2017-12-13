d6ec50cf404e71fe9295b40cb6f91b9b2f7172c4
Carsten Koenig <Carsten@gettingsharper.de>
2017-12-13 07:57:26 +0100

    optimization

--- Day13/Day13.hs
+++ Day13/Day13.hs
@@ -124,6 +124,23 @@ possibilities sc xs = filter notHit xs
         cyc = 2 * range sc - 2
 
 
+invalids :: [Scanner] -> [Int]
+invalids = foldr merge [] . map invalidDelays
+
+
+invalidDelays :: Scanner -> [Int]
+invalidDelays sc = [fst,fst + cyc..]
+  where fst = (negate $ layer sc) `mod` cyc
+        cyc = 2 * range sc - 2
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge xs@(x:xs') ys@(y:ys')
+  | x == y    = x : merge xs' ys'
+  | x <  y    = x : merge xs' ys
+  | otherwise = y : merge xs ys'
+
 ----------------------------------------------------------------------
 -- input reading/parsing
 
