9d61c5c3e5fd56028b60de0d38385ba1d2e58958
Carsten König <carsten@gettingsharper.de>
2017-12-18 19:35:22 +0100

    day 18 using STM / TChan with forked threads

--- Day18/Day18.hs
+++ Day18/Day18.hs
@@ -2,14 +2,14 @@
 
 module Main where
 
-import Control.Monad.State as ST
-
+import Control.Concurrent (forkIO, threadDelay)
+import Control.Monad (void, when)
+import Control.Monad.Reader as R
 import Data.Char (isLetter)
 import qualified Data.IntMap.Strict as IM
-import Data.Maybe (fromJust, fromMaybe)
 import qualified Data.Map.Strict as M
-import Debug.Trace
-
+import Data.Maybe (fromJust, fromMaybe)
+import Control.Concurrent.STM
 import Parser
 
 
@@ -17,10 +17,7 @@ main :: IO ()
 main = do
   prg <- readInput
 
-  let part1 = run 0 prg
-  putStrLn $ "part1 : " ++ show part1
-
-  let part2 = runPar prg
+  part2 <- run prg
   putStrLn $ "part2 : " ++ show part2
 
 
@@ -29,7 +26,7 @@ type Input = Program
 type Program = IM.IntMap Command
 
 data Command
-  = Sound Value
+  = Send Value
   | Set Register Value
   | Add Register Value
   | Mul Register Value
@@ -50,249 +47,141 @@ type Register = Char
 
 data Environment =
   Environment
-  { registers :: M.Map Char Integer
-  , programm  :: Program
-  , pointer   :: Int
-  , sound     :: Integer
-  } deriving Show
-
-
-type Runtime a = ST.State Environment a
-
-
-type RuntimePar a = ST.State EnvironmentPar a
-
-
-data EnvironmentPar =
-  EnvPar
-  { parProgramm   :: Program
-  , parRegisters0 :: M.Map Char Integer
-  , parRegisters1 :: M.Map Char Integer
-  , parPointer0   :: ParPointer
-  , parPointer1   :: ParPointer
-  , recQueue0     :: [Integer]
-  , recQueue1     :: [Integer]
-  , send1         :: Int
-  } deriving Show
-
-
-data ParPointer
-  = Receiving ProgramPointer
-  | Next      ProgramPointer
-  deriving Show
+  { registers   :: TVar (M.Map Char Signal)
+  , program     :: Program
+  , pointer     :: TVar ProgramPointer
+  , receive     :: STM (Maybe Signal)
+  , send        :: Integer -> STM ()
+  , sendCounter :: TVar Int
+  }
 
 
 type ProgramPointer = Int
 
+type Signal = Integer
 
-runPar :: Program -> Int
-runPar prg =
-  let env = ST.execState loopPar (initEnvironmentPar prg)
-  in send1 env
+type Runtime a = R.ReaderT Environment IO a
 
 
-loopPar :: RuntimePar ()
-loopPar = do
-  env <- ST.get
-  pt0 <- getPointerPar 0
-  pt1 <- getPointerPar 1
-  case (pt0, pt1) of
-    (Receiving _, Receiving _) ->
-      return ()
-    (Next i0, Receiving _) -> do
-      _ <- interpretPar 0 i0
-      loopPar
-    (Receiving _, Next i1) -> do
-      _ <- interpretPar 1 i1
-      loopPar
-    (Next i0, Next i1) -> do
-      _ <- interpretPar 0 i0
-      _ <- interpretPar 1 i1
-      loopPar
+run :: Program -> IO Int
+run prg = do
+  (env0, env1, awaitFirstSend) <- initEnvironments prg
+  th0 <- forkIO . void $ R.runReaderT (interpret 0) env0
+  -- thread 2 might run to the first receive before thread 1 even forked
+  -- so we better wait till thread2's queue got written once
+  -- need this because we detect deadlocks but simply looking for two emtpy
+  -- input queues on a read
+  awaitFirstSend
+  -- now we can start the second thread right here - this one will give
+  -- the result
+  R.runReaderT (interpret 1) env1
 
 
-interpretPar :: Int -> ProgramPointer -> RuntimePar ()
-interpretPar prgNr pt = do
-  cmd <- getCommandPar pt
-  case cmd of
-    Sound v -> do
-      f <- getValuePar prgNr v
-      send (1-prgNr) f
-      if prgNr == 1
-        then ST.modify (\e -> e { send1 = send1 e + 1})
-        else return ()
-      nextPar prgNr
-    Set r v -> do
-      f <- getValuePar prgNr v
-      setRegisterPar prgNr r f
-      nextPar prgNr
-    Add r v -> do
-      b <- getValuePar prgNr v
-      a <- getRegisterPar prgNr r
-      setRegisterPar prgNr r (a+b)
-      nextPar prgNr
-    Mul r v -> do
-      b <- getValuePar prgNr v
-      a <- getRegisterPar prgNr r
-      setRegisterPar prgNr r (a*b)
-      nextPar prgNr
-    Mod r v -> do
-      b <- getValuePar prgNr v
-      a <- getRegisterPar prgNr r
-      setRegisterPar prgNr r (a `mod` b)
-      nextPar prgNr
-    Receive r -> do
-      recv <- receivePar prgNr
-      case recv of
-        Nothing -> waitPar prgNr
-        Just v  -> do
-          setRegisterPar prgNr r v
-          nextPar prgNr
-    Jump bv ov -> do
-      b <- getValuePar prgNr bv
-      if b > 0
-      then do
-        o <- getValuePar prgNr ov
-        jumpPar prgNr (fromInteger o)
-      else
-        nextPar prgNr
-
-
-nextPar :: Int -> RuntimePar ()
-nextPar prgNr = do
-  pt <- getPointerPar prgNr
-  let pt' =
-        case pt of
-          Receiving i -> error "next from waiting" -- Next $ i+1
-          Next      i -> Next $ i+1
-  setPointerPar prgNr pt'
-
-
-jumpPar :: Int -> Int -> RuntimePar ()
-jumpPar prgNr delta = do
-  pt <- getPointerPar prgNr
-  let pt' =
-        case pt of
-          Receiving i -> error "can't jump while waiting"
-          Next      i -> Next $ i+delta
-  setPointerPar prgNr pt'
-
-
-waitPar :: Int -> RuntimePar ()
-waitPar prgNr = do
-  pt <- getPointerPar prgNr
-  let pt' =
-        case pt of
-          Receiving _ -> error "can't wait while waiting"
-          Next      i -> Receiving i
-  setPointerPar prgNr pt'
-
-
-getPointerPar :: Int -> RuntimePar ParPointer
-getPointerPar prgNr = do
-  pt <- ST.gets (\e -> if prgNr == 0 then parPointer0 e else parPointer1 e)
-  case pt of
-    Next _      -> return pt
-    Receiving i -> do
-      found <- not <$> ST.gets (\e -> if prgNr == 0 then null (recQueue0 e) else null (recQueue1 e))
-      if found
-        then do
-          ST.modify (\e -> if prgNr == 0 then e { parPointer0 = Next i} else e { parPointer1 = Next i})
-          return (Next i)
-        else return pt
-
-
-setPointerPar :: Int -> ParPointer -> RuntimePar ()
-setPointerPar prgNr pt = 
-  ST.modify (\e -> if prgNr == 0 then e { parPointer0 = pt } else e { parPointer1 = pt })
-
-
-send :: Int -> Integer -> RuntimePar ()
-send prgNr sig =
-  ST.modify (\e -> if prgNr == 0 then e { recQueue0 = recQueue0 e ++ [sig] } else e { recQueue1 = recQueue1 e ++ [sig] })
-
-
-receivePar :: Int -> RuntimePar (Maybe Integer)
-receivePar prgNr = do
-  qu <- ST.gets (\e -> if prgNr == 0 then recQueue0 e else recQueue1 e)
-  if null qu then return Nothing else do
-    let qu' = tail qu
-        sig = head qu
-    ST.modify (\e -> if prgNr == 0 then e { recQueue0 = qu' } else e { recQueue1 = qu' })
-    return $ Just sig
-
-
-run :: Int -> Program -> Integer
-run prgNr prg = ST.evalState interpret (initEnvironment prgNr prg)
-
-
-interpret :: Runtime Integer
-interpret = do
+interpret :: Int -> Runtime Int
+interpret prgNr = do
   cmd <- getCommand
   case cmd of
-    Sound v -> do
+    Send v -> do
       f <- getValue v
-      setSound f
-      next
+      sendSignal f
+      next prgNr
     Set r v -> do
       f <- getValue v
       setRegister r f
-      next
+      next prgNr
     Add r v -> do
       b <- getValue v
       a <- getRegister r
       setRegister r (a+b)
-      next
+      next prgNr
     Mul r v -> do
       b <- getValue v
       a <- getRegister r
       setRegister r (a*b)
-      next
+      next prgNr
     Mod r v -> do
       b <- getValue v
       a <- getRegister r
       setRegister r (a `mod` b)
-      next
+      next prgNr
     Receive r -> do
-      f <- getRegister r
-      if f /= 0 then getSound else next
+      sig <- receiveSignal
+      case sig of
+        Nothing ->
+          getSendCounter
+        Just val -> do
+          setRegister r val
+          next prgNr
     Jump bv ov -> do
       b <- getValue bv
       if b > 0
       then do
         o <- getValue ov
         movePointer (fromInteger o)
-        interpret
+        interpret prgNr
       else
-        next
+        next prgNr
 
 
-next :: Runtime Integer
-next = movePointer 1 >> interpret
+next :: Int -> Runtime Int
+next prgNr = movePointer 1 >> interpret prgNr
 
 
-initEnvironment :: Int -> Program -> Environment
-initEnvironment prgNr prg = Environment (M.fromList [('p', fromIntegral prgNr)]) prg 0 0
+initEnvironments :: Program -> IO (Environment, Environment, IO ())
+initEnvironments prg = do
+  qu0 <- newTQueueIO
+  qu1 <- newTQueueIO
+  env0 <- create 0 qu0 qu1
+  env1 <- create 1 qu1 qu0
+  return (env0, env1, wait qu1)
+  where
+    -- need to wait for first send to queue
+    wait qu = do
+      threadDelay 100
+      epty <- atomically $ isEmptyTQueue qu
+      when epty $ wait qu
+    isDeadLocked q0 q1 = do
+      epty0 <- isEmptyTQueue q0
+      epty1 <- isEmptyTQueue q1
+      return $ epty0 && epty1
+    sendTo sc queueOther sig = do
+      modifyTVar' sc (+ 1)
+      writeTQueue queueOther sig
+    tryReceive prgNr queue isDeadl = do
+      deadl <- isDeadl
+      if deadl
+        then tryReadTQueue queue
+        else Just <$> readTQueue queue
+    create prgNr queue queueOther = do
+      ptr  <- newTVarIO 0
+      regs <- newTVarIO (M.fromList [('p', fromIntegral prgNr)])
+      scnt <- newTVarIO 0
+      return $ Environment regs prg ptr (tryReceive prgNr queue (isDeadLocked queue queueOther)) (sendTo scnt queueOther) scnt
 
 
-initEnvironmentPar :: Program -> EnvironmentPar
-initEnvironmentPar prg = EnvPar
-  prg
-  (M.fromList [('p', fromIntegral 0)])
-  (M.fromList [('p', fromIntegral 1)])
-  (Next 0)
-  (Next 0)
-  []
-  []
-  0
+sendSignal :: Signal -> Runtime ()
+sendSignal sig =
+  R.asks send >>= lift . atomically . ($ sig)
+
+
+receiveSignal :: Runtime (Maybe Signal)
+receiveSignal = R.asks receive >>= lift . atomically
+
+
+getSendCounter :: Runtime Int
+getSendCounter = R.asks sendCounter >>= lift . atomically . readTVar
 
 
 getCommand :: Runtime Command
-getCommand = ST.gets (\e -> programm e IM.! pointer e)
+getCommand = do
+  prg <- R.asks program
+  pt  <- R.asks pointer >>= lift . atomically . readTVar
+  return $ prg IM.! pt
 
 
-getCommandPar :: ProgramPointer -> RuntimePar Command
-getCommandPar pt = ST.gets (\e -> parProgramm e IM.! pt)
+movePointer :: Int -> Runtime ()
+movePointer delta =
+  R.asks pointer >>= lift . atomically . (`modifyTVar'` (+ delta))
 
 
 getValue :: Value -> Runtime Integer
@@ -300,40 +189,19 @@ getValue (RegValue r) = getRegister r
 getValue (IntValue i) = return (fromIntegral i)
 
 
-getValuePar :: Int -> Value -> RuntimePar Integer
-getValuePar prgNr (RegValue r) = getRegisterPar prgNr r
-getValuePar _     (IntValue i) = return (fromIntegral i)
-
-
 getRegister :: Register -> Runtime Integer
-getRegister r = ST.gets (\e -> fromMaybe 0 $ M.lookup r $ registers e)
-
-
-getRegisterPar :: Int -> Register -> RuntimePar Integer
-getRegisterPar prgNr r = ST.gets (\e -> fromMaybe 0 $ M.lookup r $ if prgNr == 0 then parRegisters0 e else parRegisters1 e)
-
-
-movePointer :: Int -> Runtime ()
-movePointer delta = ST.modify (\e -> e { pointer = pointer e + delta })
+getRegister r = do
+  regs <- R.asks registers >>= lift . atomically . readTVar
+  return $ fromMaybe 0 $ M.lookup r regs
 
 
 setRegister :: Register -> Integer -> Runtime ()
-setRegister r v = ST.modify (\e -> e { registers = M.insert r v $ registers e })
+setRegister r v =
+  R.asks registers >>= lift . atomically . (`modifyTVar'` M.insert r v)
 
 
-setRegisterPar :: Int -> Register -> Integer -> RuntimePar ()
-setRegisterPar prgNr r v = ST.modify (\e -> if prgNr == 0
-                                            then e { parRegisters0 = M.insert r v $ parRegisters0 e }
-                                            else e { parRegisters1 = M.insert r v $ parRegisters1 e })
-
-
-getSound :: Runtime Integer
-getSound = ST.gets sound
-
-
-setSound :: Integer -> Runtime ()
-setSound f = ST.modify (\e -> e { sound = f })
-
+----------------------------------------------------------------------
+-- input / parsing
 
 readInput :: IO Input
 readInput = parseInput <$> readFile "input.txt"
@@ -345,7 +213,7 @@ parseInput = IM.fromList . zip [0..] . map (fromJust . eval commandP) . lines
 
 commandP :: Parser Command
 commandP = parseOneOf
-  [ unP  Sound   "snd" valueP
+  [ unP  Send    "snd" valueP
   , binP Set     "set" registerP valueP
   , binP Add     "add" registerP valueP
   , binP Mul     "mul" registerP valueP
--- Day18/readme.md
+++ Day18/readme.md
@@ -75,4 +75,4 @@ Once both of your programs have terminated (regardless of what caused them to do
 
 ## Solution
 
-- in [Haskell](./Day18.hs) - right now it's a bit messed up - if I find the time I should rewrite this to STM
+- in [Haskell](./Day18.hs) - using STM right now but not 100% sure if the deadlock detection is actually good
