1dda8fce4e14431c13db5fdafdb9792012ece90e
Carsten Koenig <Carsten@gettingsharper.de>
2017-12-18 06:20:14 +0100

    day 18: parsed input

--- Day18/Day18.hs
+++ Day18/Day18.hs
@@ -2,6 +2,13 @@
 
 module Main where
 
+import Data.Char (isLetter)
+import qualified Data.IntMap.Strict as IM
+import Data.Maybe (fromJust)
+
+
+import Parser
+
 
 main :: IO ()
 main = do
@@ -9,8 +16,56 @@ main = do
   putStrLn $ "solve me"
 
 
-type Input = ()
+type Input = Program
+
+type Program = IM.IntMap Command
+
+data Command
+  = Sound Value
+  | Set Register Value
+  | Add Register Value
+  | Mul Register Value
+  | Mod Register Value
+  | Receive Value
+  | Jump Value Value
+  deriving Show
+
+
+data Value
+  = RegValue Register
+  | IntValue Int
+  deriving Show
+
+
+type Register = Char
 
 
 readInput :: IO Input
-readInput = const () <$> readFile "input.txt"
+readInput = parseInput <$> readFile "input.txt"
+
+
+parseInput :: String -> Input
+parseInput = IM.fromList . zip [0..] . map (fromJust . eval commandP) . lines
+
+
+commandP :: Parser Command
+commandP = parseOneOf
+  [ unP  Sound   "snd" valueP
+  , binP Set     "set" registerP valueP
+  , binP Add     "add" registerP valueP
+  , binP Mul     "mul" registerP valueP
+  , binP Mod     "mod" registerP valueP
+  , unP  Receive "rcv" valueP
+  , binP Jump    "jgz" valueP valueP
+  ]
+  where
+    unP  c s p     = c <$> (parseString s *> ignoreWhiteSpace *> p <* ignoreWhiteSpace)
+    binP c s pa pb = c <$> (parseString s *> ignoreWhiteSpace *> pa) <*> (ignoreWhiteSpace *> pb <* ignoreWhiteSpace)
+
+
+valueP :: Parser Value
+valueP = parseOneOf [ RegValue <$> registerP, IntValue <$> parseInt ]
+
+
+registerP :: Parser Register
+registerP = parsePred isLetter <* ignoreWhiteSpace
