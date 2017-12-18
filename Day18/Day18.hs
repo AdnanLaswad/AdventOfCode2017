703f44d019fd5a7b85fc82a30c0849dce562c23a
Carsten Koenig <Carsten@gettingsharper.de>
2017-12-18 07:48:31 +0100

    Day18 star 1

--- Day18/Day18.hs
+++ Day18/Day18.hs
@@ -2,18 +2,23 @@
 
 module Main where
 
+import Control.Monad.State as ST
+
 import Data.Char (isLetter)
 import qualified Data.IntMap.Strict as IM
-import Data.Maybe (fromJust)
-
+import Data.Maybe (fromJust, fromMaybe)
+import qualified Data.Map.Strict as M
+import Debug.Trace
 
 import Parser
 
 
 main :: IO ()
 main = do
-  inp <- readInput
-  putStrLn $ "solve me"
+  prg <- readInput
+
+  let part1 = run prg
+  putStrLn $ "part1 : " ++ show part1
 
 
 type Input = Program
@@ -38,6 +43,100 @@ data Value
 
 
 type Register = Char
+
+
+data Environment =
+  Environment
+  { registers :: M.Map Char Integer
+  , programm  :: Program
+  , pointer   :: Int
+  , sound     :: Integer
+  } deriving Show
+
+
+type Runtime a = ST.State Environment a
+
+
+run :: Program -> Integer
+run prg = ST.evalState interpret (initEnvironment prg)
+
+
+interpret :: Runtime Integer
+interpret = do
+  cmd <- getCommand
+  case cmd of
+    Sound v -> do
+      f <- getValue v
+      setSound f
+      next
+    Set r v -> do
+      f <- getValue v
+      setRegister r f
+      next
+    Add r v -> do
+      b <- getValue v
+      a <- getRegister r
+      setRegister r (a+b)
+      next
+    Mul r v -> do
+      b <- getValue v
+      a <- getRegister r
+      setRegister r (a*b)
+      next
+    Mod r v -> do
+      b <- getValue v
+      a <- getRegister r
+      setRegister r (a `mod` b)
+      next
+    Receive v -> do
+      f <- getValue v
+      if f /= 0 then getSound else next
+    Jump bv ov -> do
+      b <- getValue bv
+      if b > 0
+      then do
+        o <- getValue ov
+        movePointer (fromInteger o)
+        interpret
+      else
+        next
+
+
+next :: Runtime Integer
+next = movePointer 1 >> interpret
+
+
+initEnvironment :: Program -> Environment
+initEnvironment prg = Environment M.empty prg 0 0
+
+
+getCommand :: Runtime Command
+getCommand = ST.gets (\e -> programm e IM.! pointer e)
+
+
+getValue :: Value -> Runtime Integer
+getValue (RegValue r) = getRegister r
+getValue (IntValue i) = return (fromIntegral i)
+
+
+getRegister :: Register -> Runtime Integer
+getRegister r = ST.gets (\e -> fromMaybe 0 $ M.lookup r $ registers e)
+
+
+movePointer :: Int -> Runtime ()
+movePointer delta = ST.modify (\e -> e { pointer = pointer e + delta })
+
+
+setRegister :: Register -> Integer -> Runtime ()
+setRegister r v = ST.modify (\e -> e { registers = M.insert r v $ registers e })
+
+
+getSound :: Runtime Integer
+getSound = ST.gets sound
+
+
+setSound :: Integer -> Runtime ()
+setSound f = ST.modify (\e -> e { sound = f })
 
 
 readInput :: IO Input
