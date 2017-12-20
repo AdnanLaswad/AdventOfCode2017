097f193d60e79b978b8fcabd6b9d5e977cdd2a6c
Carsten Koenig <Carsten@gettingsharper.de>
2017-12-20 06:21:51 +0100

    day 20 star 1

--- Day20/Day20.hs
+++ Day20/Day20.hs
@@ -2,6 +2,10 @@
 
 module Main where
 
+import Data.Function (on)
+import Data.List (maximumBy)
+import Data.Maybe (fromJust)
+import Parser
 
 main :: IO ()
 main = do
@@ -9,8 +13,69 @@ main = do
   putStrLn $ "solve me"
 
 
-type Input = ()
+type Input = [Particle]
+
+
+type Position = (Int,Int,Int)
+type Velocity = (Int,Int,Int)
+type Acceleration = (Int,Int,Int)
+
+
+data Particle =
+  Particle
+  { number       :: Int
+  , position     :: Position
+  , velocity     :: Velocity
+  , acceleration :: Acceleration
+  } deriving (Show)
+
+
+instance Eq Particle where
+  pa == pb = pa `compare` pb == EQ
+
+
+instance Ord Particle where
+  (Particle _ pa va aa) `compare` (Particle _ pb vb ab) =
+    case norm aa `compare` norm ab of
+      EQ -> case norm va `compare` norm vb of
+              EQ -> norm pa `compare` norm pb
+              o  -> o
+      o  -> o
+
+
+dist :: Num a => (a,a,a) -> (a,a,a) -> a
+dist (x,y,z) (x',y',z') = abs (x-x') + abs (y-y') + abs (z-z')
+
+
+norm :: Num a => (a,a,a) -> a
+norm = dist (0,0,0)
 
 
 readInput :: IO Input
-readInput = const () <$> readFile "input.txt"
+readInput = zipWith (\i -> fromJust . eval (particleP i)) [0..]  . lines <$> readFile "input.txt"
+
+
+particleP :: Int -> Parser Particle
+particleP nr = Particle nr <$> posP <* parseString ", " <*> velP <* parseString ", " <*> accP
+
+
+posP :: Parser Position
+posP =
+  (,,) <$> (parseString "p=<" *> parseInt <* parseString ",")
+       <*> (parseInt <* parseString ",")
+       <*> (parseInt <* parseString ">")
+
+
+velP :: Parser Velocity
+velP =
+  (,,) <$> (parseString "v=<" *> parseInt <* parseString ",")
+       <*> (parseInt <* parseString ",")
+       <*> (parseInt <* parseString ">")
+
+
+accP :: Parser Acceleration
+accP =
+  (,,) <$> (parseString "a=<" *> parseInt <* parseString ",")
+       <*> (parseInt <* parseString ",")
+       <*> (parseInt <* parseString ">")
+
