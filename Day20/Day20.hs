575725d519dcc50444c2475e1eeee02f22a498e2
Carsten Koenig <Carsten@gettingsharper.de>
2017-12-20 07:21:12 +0100

    Day20 part 2 (a bit a cheat)

--- Day20/Day20.hs
+++ Day20/Day20.hs
@@ -3,14 +3,25 @@
 module Main where
 
 import Data.Function (on)
-import Data.List (maximumBy)
-import Data.Maybe (fromJust)
+import Data.List (maximumBy, sortBy, groupBy, (\\))
+import Data.Maybe (fromJust, maybeToList)
+import qualified Data.Set as S
 import Parser
+import Debug.Trace
 
 main :: IO ()
 main = do
   inp <- readInput
-  putStrLn $ "solve me"
+
+  putStrLn $ "part 1: " ++ show (number $ minimum inp)
+
+  -- simulate says 420!
+  -- let ps' = simulate inp
+  -- print ps'
+
+  -- but this says 423 - SHIIT
+  let part2 = length $ removeCollisions inp
+  putStrLn $ "part 2: " ++ show part2
 
 
 type Input = [Particle]
@@ -49,6 +60,63 @@ dist (x,y,z) (x',y',z') = abs (x-x') + abs (y-y') + abs (z-z')
 
 norm :: Num a => (a,a,a) -> a
 norm = dist (0,0,0)
+
+
+tick :: Particle -> Particle
+tick (Particle n p v a) = (Particle n p' v' a)
+  where v' = add v a
+        p' = add p v'
+        add (x,y,z) (x',y',z') = (x+x',y+y',z+z')
+
+
+areColliding :: [Particle] -> [Particle]
+areColliding ps = concat [[a,b] | (a,b) <- pickTwo ps, position a == position b ]
+
+
+simulate :: [Particle] -> [Particle]
+simulate ps =
+  let ps' = ps \\ areColliding ps
+  in trace (show $ length ps') $ simulate (tick <$> ps')
+
+
+collide :: Particle -> Particle -> Maybe Int
+collide pa pb = go 0 (dist (position pa) (position pb)) pa pb
+  where go t oldD px py =
+          let px' = tick px
+              py' = tick py
+              d   = dist (position px) (position py)
+          in if d == 0
+             then Just t
+             else if oldD < d then Nothing else go (t+1) d px' py'
+
+
+collisions :: [Particle] -> [(Particle,Particle)]
+collisions ps =
+  let grps = groupBy ((==) `on` \(t,_,_) -> t) $ sortBy (compare `on` \(t,_,_) -> t)
+             [ (t,pa,pb) | (pa,pb) <- pickTwo ps
+                         , t <- maybeToList (collide pa pb) ]
+  in if null grps then [] else map (\(_,a,b) -> (a,b)) $ head grps
+
+
+removeCollisions :: [Particle] -> [Particle]
+removeCollisions ps =
+  let cols = map number $ concatMap (\(a,b) -> [a,b]) $ collisions ps
+  in if null cols
+     then ps
+     else let ps' = filter (\p -> not (number p `elem` cols)) ps
+          in removeCollisions ps'
+
+
+pickTwo :: [a] -> [(a,a)]
+pickTwo as = do
+  (a,as') <- pick as
+  (b,_)   <- pick as'
+  return (a,b)
+
+
+pick :: [a] -> [(a,[a])]
+pick [] = []
+pick (a:as) = (a,as) : pick as
 
 
 readInput :: IO Input
